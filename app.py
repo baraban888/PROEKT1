086f5ea89105e5713e8b9c9ba91aaeebb18ec003
baraban888 <1975bahat@gmail.com>
2025-07-10 20:21:36 +0200

    one commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(name)
+CORS(app)
+
+@app.router("/api/hello")
+def hello( ):
+    return jsonify(message="Hay from Flask")
