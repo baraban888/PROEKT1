9b1b148da171595225b9161858f4c173ce6d68af
baraban888 <1975bahat@gmail.com>
2025-07-12 23:43:53 +0200

    updated HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/api/hello")
-def hello( ):
-    return jsonify(message="Hay from Flask")
+@app.route("/")
+def home( ):
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
